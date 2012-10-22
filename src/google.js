1d9cca3c02b52e1e0d11f207c17fdd353c787b29
Jason Barrie Morley <jbmorley@mac.com>
2012-10-22 22:40:47 +0100

    Removing the unused file upload support.

--- src/google.js
+++ src/google.js
@@ -24,13 +24,9 @@ function checkAuth() {
  */
 function handleAuthResult(authResult) {
   var authButton = document.getElementById('authorizeButton');
-  var filePicker = document.getElementById('filePicker');
   authButton.style.display = 'none';
-  filePicker.style.display = 'none';
   if (authResult && !authResult.error) {
     // Access token has been successfully retrieved, requests can be sent to the API.
-    filePicker.style.display = 'block';
-    filePicker.onchange = uploadFile;
   } else {
     // No access token could be retrieved, show the button to start the authorization flow.
     authButton.style.display = 'block';
@@ -108,66 +104,4 @@ function retrieveAllFiles(callback) {
     'params': {'maxResults': '100'}
   });
   retrievePageOfFiles(initialRequest, []);
-}
-
-/**
- * Insert new file.
- *
- * @param {File} fileData File object to read data from.
- * @param {Function} callback Function to call when the request is complete.
- */
-function insertFile(fileData, callback) {
-  const boundary = '-------314159265358979323846';
-  const delimiter = "\r\n--" + boundary + "\r\n";
-  const close_delim = "\r\n--" + boundary + "--";
-
-  var reader = new FileReader();
-  reader.readAsBinaryString(fileData);
-  reader.onload = function(e) {
-    var contentType = fileData.type || 'application/octet-stream';
-    var metadata = {
-      'title': fileData.name,
-      'mimeType': contentType
-    };
-
-    var base64Data = btoa(reader.result);
-    var multipartRequestBody =
-        delimiter +
-        'Content-Type: application/json\r\n\r\n' +
-        JSON.stringify(metadata) +
-        delimiter +
-        'Content-Type: ' + contentType + '\r\n' +
-        'Content-Transfer-Encoding: base64\r\n' +
-        '\r\n' +
-        base64Data +
-        close_delim;
-
-    var request = gapi.client.request({
-        'path': '/upload/drive/v2/files',
-        'method': 'POST',
-        'params': {'uploadType': 'multipart'},
-        'headers': {
-          'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
-        },
-        'body': multipartRequestBody});
-    if (!callback) {
-      callback = function(file) {
-        console.log(file)
-      };
-    }
-    request.execute(callback);
-  }
-}
-
-
-/**
- * Start the file upload.
- *
- * @param {Object} evt Arguments from the file selector.
- */
-function uploadFile(evt) {
-  gapi.client.load('drive', 'v2', function() {
-    var file = evt.target.files[0];
-    insertFile(file);
-  });
 }
