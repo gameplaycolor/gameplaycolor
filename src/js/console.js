6067e7b744c3c4d70cec415e3c9fc2afa7f31c6c
Jason Barrie Morley <jbmorley@mac.com>
2012-10-25 07:05:05 +0100

    Improving orientation change support in the console to make it easier to update the layout on rotation.

--- src/js/console.js
+++ src/js/console.js
@@ -8,6 +8,11 @@
     VISIBLE: 0,
     HIDDEN:  1,
   };
+  
+  App.Console.Orientation = {
+    PORTRAIT:  0,
+    LANDSCAPE: 1,
+  };
 
   jQuery.extend(
     App.Console.prototype, {
@@ -17,11 +22,49 @@
       
         self.element = $('#screen-console');
         self.state = App.Console.State.VISIBLE;
-      
+
+        // Update the initial orientation and watch for changes.        
+        self.orientationChange(function(orientation) {
+          console.log("Orientation changed: " + orientation);
+        });
+
+        // Configure the game button.
         self.games = new App.Controls.Button('#control-games', { 'touchUpInside': function() {
           self.toggle();
         }});
+        $('#LCD').click(function() {
+          self.toggle();        
+        });
         
+      },
+      
+      orientationChange: function(callback) {
+        var self = this;
+        
+        // Determine the initial orientation.
+        self.orientation = App.Console.Orientation.PORTRAIT;
+        if ($(window).width() > 320) {
+          self.orientation = App.Console.Orientation.LANDSCAPE;
+        }
+        
+        // Orientation events (via window size).
+        $(window).resize(function() {
+          var width = $(window).width();
+          var orientation = self.orientation;
+          if (width > 320) {
+            orientation = App.Console.Orientation.LANDSCAPE;
+          } else {
+            orientation = App.Console.Orientation.PORTRAIT;
+          }
+          
+          // Only execute the callback if the orientation has actually changed.
+          if (orientation != self.orientation) {
+            self.orientation = orientation;
+            callback(self.orientation);
+          }
+          
+        });
+
       },
       
       hide: function() {
