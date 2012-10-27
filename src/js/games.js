26b41b9cf6e3def4e8336b49e80a2b8563ff3b40
Jason Barrie Morley <jbmorley@mac.com>
2012-10-27 14:36:10 +0100

    Introducing a new library.

--- src/js/games.js
+++ src/js/games.js
@@ -14,27 +14,8 @@
       self.empty = $('#screen-empty');
       self.grid = new App.Grid();
       self.items = new Array();
-      
-      self.grid.dataSource = {
-        'count' : function() {
-           return self.items.length;
-         },
-        'titleForIndex': function(index) {
-          return self.items[index].title;
-        },
-        'didSelectItemForRow': function(index) {
-          // TODO This needs to be more elegant.
-          // Do we want a progress indicator for this?
-          downloadFile(self.items[index], function(data) {
-            gb_Insert_Cartridge_Data(data, true);
-            // TODO This should be shown using the correct API.
-            $('#screen-console').animate({
-              top: '0'
-            }, 300, function() {
-            });
-          });
-        },
-      };
+      self.library = new App.Library();
+      self.grid.dataSource = self.library;
       
       // Next.
       self.next = new App.Controls.Button('#control-next', {
@@ -54,63 +35,15 @@
     
     update: function() {
       var self = this;
-      self.empty.show();
       
-      // Update the files.
-      retrieveAllFiles(function(result) {
-        self.items = new Array();
-        self.empty.hide();
-        for (var i=0; i<result.length; i++) {
-          self.items.push(result[i]);
-        }
+      // Update the library.
+      self.empty.show();
+      self.library.update(function() {
         self.grid.reloadData();
+        self.empty.hide();
       });
-            
+                  
     },
-/*
-        var list = $('#list-games')
-        
-        var count = 0;
-        var row = 0;
-        var column = 0;
-        
-        var ROWS = 3;
-        var WIDTH  = 120;
-        var HEIGHT = 120;
-        var MARGIN = 30;
-        
-        for (var i=0; i<result.length; i++) {
-
-          row = count % ROWS;
-          col = Math.floor(count / ROWS);
-          
-          var game = $('<div class="game">');
-          game.html(result[i].title)
-          game.css('top', (HEIGHT + MARGIN) * row);
-          game.css('left', (WIDTH + MARGIN) * col);
-          
-          // Ugly way around capturing the callback parameter.
-          // This might be more elegantly served if we had a javascript
-          // element backing each list element.
-          (function() {
-            var m = result[i];
-            game.click(function() {
-              downloadFile(m, function(data) {
-                gb_Insert_Cartridge_Data(data, true);
-                gb_Run();
-                $('#screen-console').animate({
-                  top: '0'
-                }, 300, function() {
-                });
-              });
-            });
-          })();
-          list.append(game);
-
-          count += 1;
-        }
-        
-*/
 
   });
 
