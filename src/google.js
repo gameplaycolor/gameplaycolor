f07ccd1f1753214250c4cb98521f9df30539831f
Jason Morley <jbmorley@inseven.co.uk>
2012-12-22 00:12:47 +0000

    Cleaning up the Google Drive implementation a little.

--- src/google.js
+++ src/google.js
@@ -1,45 +1,103 @@
+(function($) {
 
-CLIENT_ID = '662240015525';
-SCOPES = 'https://www.googleapis.com/auth/drive';
+  // TODO Consider taking the client id
+  App.Drive = function() {
+    this.init();
+  };
 
-/**
- * Called when the client library is loaded to start the auth flow.
- */
+  App.Drive.instance = undefined;
+
+  App.Drive.getInstance = function() {
+    if (App.Drive.instance === undefined) {
+      App.Drive.instance = new App.Drive();
+    }
+    return App.Drive.instance;
+  };
+
+  jQuery.extend(App.Drive.prototype, {
+        
+      init: function() {
+        var self = this;
+      },
+
+      update: function() {
+        var self = this;
+
+        // TODO This should be a state engine which guarantees we only fetch
+        // the SDK once even if update is called multiple times.
+
+        // Fetch the Google Drive client configuration if we're online.
+        // This effectively kicks off the Google SDK load and the update to the
+        // game library.
+        if (navigator.onLine) {
+          jQuery.getJSON("settings.json", function(data) {
+            self.clientID = data["client_id"];
+            self.scopes = data["scopes"];
+            self.loadSDK();
+          });
+        }
+      },
+
+      loadSDK: function() {
+        var self = this;
+
+        // Load the Google SDK
+        // This will call the function handleClientLoad when complete.
+        (function(d){
+           var js, id = 'google-sdk', ref = d.getElementsByTagName('script')[0];
+           if (d.getElementById(id)) {return;}
+           js = d.createElement('script'); js.id = id; js.async = true;
+           js.src = "https://apis.google.com/js/client.js?onload=handleClientLoad";
+           ref.parentNode.insertBefore(js, ref);
+         }(document));
+
+      },
+
+      loadComplete: function() {
+        var self = this;
+        setTimeout(function() {
+          gapi.auth.authorize(
+            {
+              'client_id': self.clientID,
+              'scope': self.scopes,
+              'immediate': true
+            },
+            self.handleAuthenticationResult);
+        }, 0);
+      },
+
+      handleAuthenticationResult: function(authResult) {
+        var self = this;
+
+        // TODO This should change state...
+        var authButton = document.getElementById('authorizeButton');
+        authButton.style.display = 'none';
+        if (authResult && !authResult.error) {
+          // Access token has been successfully retrieved, requests can be sent to the API.
+        } else {
+          // No access token could be retrieved, show the button to start the authorization flow.
+          authButton.style.display = 'block';
+          authButton.onclick = function() {
+              gapi.auth.authorize(
+                {
+                  'client_id': self.clientID,
+                  'scope': self.scopes,
+                  'immediate': false
+                },
+                handleAuthResult);
+          };
+        }
+
+      },
+
+  });
+
+})(jQuery);
+
+// Called when the client library is loaded to start the auth flow.
 function handleClientLoad() {
-  window.setTimeout(checkAuth, 1);
+  App.Drive.getInstance().loadComplete();
 }
-
-/**
- * Check if the current user has authorized the application.
- */
-function checkAuth() {
-  gapi.auth.authorize(
-      {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': true},
-      handleAuthResult);
-}
-
-/**
- * Called when authorization server replies.
- *
- * @param {Object} authResult Authorization result.
- */
-function handleAuthResult(authResult) {
-  var authButton = document.getElementById('authorizeButton');
-  authButton.style.display = 'none';
-  if (authResult && !authResult.error) {
-    // Access token has been successfully retrieved, requests can be sent to the API.
-  } else {
-    // No access token could be retrieved, show the button to start the authorization flow.
-    authButton.style.display = 'block';
-    authButton.onclick = function() {
-        gapi.auth.authorize(
-            {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false},
-            handleAuthResult);
-    };
-  }
-
-}
-
 
 /**
  * Download a file's content.
--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,9 @@
       self.running = false;
       self.store = new App.Store();
 
+      // Initialize Google Drive
+      App.Drive.getInstance().update();
+
       self.games = new App.Games(function() {
         self.running == true;
         self.didLoad();
@@ -38,16 +41,7 @@
       
       self.checkForUpdate();
       
-      // Load the Google SDK
-      if (navigator.onLine) {
-        (function(d){
-           var js, id = 'google-sdk', ref = d.getElementsByTagName('script')[0];
-           if (d.getElementById(id)) {return;}
-           js = d.createElement('script'); js.id = id; js.async = true;
-           js.src = "https://apis.google.com/js/client.js?onload=handleClientLoad";
-           ref.parentNode.insertBefore(js, ref);
-         }(document));
-      }
+
 
     },
     
