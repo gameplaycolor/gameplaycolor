123b1d3bff58eac9676179649edd251230b64a30
Jason Morley <jbmorley@inseven.co.uk>
2012-12-22 00:05:36 +0000

    Adding deployment scripts to make it easier to automatically deploy to production and test environments.

---
+++ build/deploy-beta.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export TARGET="zyloid:~/sites/jbmorley/gameboy-beta"
+export ROOT=../src
+
+rsync -avPe ssh $ROOT/*.html $TARGET
+rsync -avPe ssh $ROOT/*.js   $TARGET
+rsync -avPe ssh $ROOT/js     $TARGET
+rsync -avPe ssh $ROOT/jsgb   $TARGET
+rsync -avPe ssh $ROOT/css    $TARGET
+rsync -avPe ssh $ROOT/images $TARGET
+
+rsync -avPe ssh $ROOT/settings-beta.json $TARGET/settings.json
---
+++ build/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export TARGET="zyloid:~/sites/jbmorley/gameboy"
+export ROOT=../src
+
+rsync -avPe ssh $ROOT/*.html     $TARGET
+rsync -avPe ssh $ROOT/*.js       $TARGET
+rsync -avPe ssh $ROOT/js         $TARGET
+rsync -avPe ssh $ROOT/jsgb       $TARGET
+rsync -avPe ssh $ROOT/css        $TARGET
+rsync -avPe ssh $ROOT/images     $TARGET
+rsync -avPe ssh $ROOT/*.manifest $TARGET
+
+rsync -avPe ssh $ROOT/settings.json $TARGET/settings.json
