eb95fd1a3314a50aaf684a8a9ed07ffed5442662
Jason Barrie Morley <jbmorley@inseven.co.uk>
2013-01-01 21:49:35 +0000

    Changing the way touches are recognised on the d-pad to make it easier to use.

--- readme.md
+++ readme.md
@@ -4,17 +4,15 @@
 
 ### Beta
 
-1. Fix the bug with the dpad
-
-2. Delete files which have been removed from Google Drive
-3. Ignore files in the Trash
-4. Update the details of the items in the cache
-5. Should we always show all items in the cache but only let users select downloaded ones when offline?
-6. Disable saving for the time being
-7. Show loading text when the game list is loading
-8. Show some hint text when the game list is empty
-9. Fix the grid view hit test
-10. Fix the off-by-one in the grid view paging calculations
+1. Delete files which have been removed from Google Drive
+2. Ignore files in the Trash
+3. Update the details of the items in the cache
+4. Should we always show all items in the cache but only let users select downloaded ones when offline?
+5. Disable saving for the time being
+6. Show loading text when the game list is loading
+7. Show some hint text when the game list is empty
+8. Fix the grid view hit test
+9. Fix the off-by-one in the grid view paging calculations
 
 ### Gamma
 
--- src/js/pad.js
+++ src/js/pad.js
@@ -20,12 +20,15 @@
     UPLEFT:    8
   };
 
+  App.Controls.Pad.DIAGONAL_THRESHOLD = 30;
+
   jQuery.extend(
     App.Controls.Pad.prototype,
     App.Control.prototype, {
 
     onCreate: function() {
       var self = this;
+      self.touches = 0;
 
       self.up    = false;
       self.down  = false;
@@ -35,7 +38,7 @@
 
     width: function() {
       var self = this;
-      return self.element.width(); 
+      return self.element.width();
     },
 
     height: function() {
@@ -46,59 +49,83 @@
     onTouchEvent: function(state, position, timestamp) {
       var self = this;
 
-      var width = self.element.height();
-      var height = self.element.width();
-
-      switch(state) {
-        case App.Control.Touch.START:
-        case App.Control.Touch.MOVE:
-
-          var upper = (width*2/3) + 10;
-          var lower = (width*1/3) - 10;
-
-          var posX = 0;
-          if (position.x > upper) {
-            posX = 1;
-          } else if (position.x > lower) {
-            posX = 0;
-          } else {
-            posX = -1;
-          }
-
-          var posY = 0;
-          if (position.y > upper) {
-            posY = 1;
-          } else if (position.y > lower) {
-            posY = 0;
-          } else {
-            posY = -1;
-          }
-
-          if (posX == -1 && posY == -1) {
-            self.setState(App.Control.State.UPLEFT);
-          } else if (posX == 0 && posY == -1) {
-            self.setState(App.Control.State.UP);
-          } else if (posX == 1 && posY == -1) {
-            self.setState(App.Control.State.UPRIGHT);
-          } else if (posX == -1 && posY == 0) {
-            self.setState(App.Control.State.LEFT);
-          } else if (posX == 0 && posY == 0) {
-            self.setState(App.Control.State.DEFAULT);
-          } else if (posX == 1 && posY == 0) {
-            self.setState(App.Control.State.RIGHT);
-          } else if (posX == -1 && posY == 1) {
-            self.setState(App.Control.State.DOWNLEFT);
-          } else if (posX == 0 && posY == 1) {
-            self.setState(App.Control.State.DOWN);
-          } else if (posX == 1 && posY == 1) {
-            self.setState(App.Control.State.DOWNRIGHT);
-          }
-
-          break;
-        case App.Control.Touch.END:
-          self.setState(App.Control.State.DEFAULT);
-          break;
+      if (state === App.Control.Touch.START) {
+        self.touches = 1;
+        self.processTouchEvent(state, position, timestamp);
+      } else if (state === App.Control.Touch.MOVE) {
+        if (self.touches === 1) {
+          self.processTouchEvent(state, position, timestamp);
+        }
+      } else if (state === App.Control.Touch.END) {
+        self.setState(App.Control.State.DEFAULT);
+        self.touches = 0;
       }
+
+    },
+
+    processTouchEvent: function(state, position, timestamp) {
+      var self = this;
+
+      var halfWidth = self.element.width() / 2;
+      var halfHeight = self.element.height() / 2;
+
+      // Mapping this into a less than obvious coordinate space because it seemed
+      // a good idea at the time.
+      var x = (halfWidth - position.x) * -1;
+      var y = halfHeight - position.y;
+
+      // Check for the corners (diagonal movement).
+      var diagonal = false;
+      if ((Math.abs(x) >= (halfWidth - App.Controls.Pad.DIAGONAL_THRESHOLD)) &&
+          (Math.abs(y) >= (halfHeight - App.Controls.Pad.DIAGONAL_THRESHOLD))) {
+        diagonal = true;
+      }
+
+      if (x < 0) {
+        // Left half.
+        if (y < 0) {
+          // Bottom-left quadrant.
+          if (diagonal) {
+            self.setState(App.Controls.Pad.State.DOWNLEFT);
+          } else if (Math.abs(y) < Math.abs(x)) {
+            self.setState(App.Controls.Pad.State.LEFT);
+          } else {
+            self.setState(App.Controls.Pad.State.DOWN);
+          }
+        } else {
+          // Top-left quadrant.
+          if (diagonal) {
+            self.setState(App.Controls.Pad.State.UPLEFT);
+          } else if (Math.abs(y) < Math.abs(x)) {
+            self.setState(App.Controls.Pad.State.LEFT);
+          } else {
+            self.setState(App.Controls.Pad.State.UP);
+          }
+
+        }
+      } else {
+        // Right half.
+        if (y < 0) {
+          // Bottom-right quadrant.
+          if (diagonal) {
+            self.setState(App.Controls.Pad.State.DOWNRIGHT);
+          } else if (Math.abs(y) < Math.abs(x)) {
+            self.setState(App.Controls.Pad.State.RIGHT);
+          } else {
+            self.setState(App.Controls.Pad.State.DOWN);
+          }
+        } else {
+          // Top-right quadrant.
+          if (diagonal) {
+            self.setState(App.Controls.Pad.State.UPRIGHT);
+          } else if (Math.abs(y) < Math.abs(x)) {
+            self.setState(App.Controls.Pad.State.RIGHT);
+          } else {
+            self.setState(App.Controls.Pad.State.UP);
+          }
+        }
+      }
+
     },
 
     setState: function(state) {
