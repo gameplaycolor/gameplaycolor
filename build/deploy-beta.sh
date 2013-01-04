e79c43017b762d10fb6cd7238e24c6ab1658f6d8
Jason Barrie Morley <jbmorley@inseven.co.uk>
2013-01-04 23:14:22 +0000

    Shuffling around the deployment scripts to make things a little more logical.

--- build/deploy-beta.sh
+++ build/deploy-beta.sh
@@ -3,12 +3,10 @@
 export TARGET="zyloid:~/sites/jbmorley/gameboy-beta"
 export ROOT=../src
 
-rsync -avPe ssh $ROOT/*.txt  $TARGET
 rsync -avPe ssh $ROOT/*.html $TARGET
-rsync -avPe ssh $ROOT/*.js   $TARGET
 rsync -avPe ssh $ROOT/js     $TARGET
 rsync -avPe ssh $ROOT/jsgb   $TARGET
 rsync -avPe ssh $ROOT/css    $TARGET
 rsync -avPe ssh $ROOT/images $TARGET
 
-rsync -avPe ssh $ROOT/settings-beta.json $TARGET/settings.json
+rsync -avPe ssh settings-beta.json $TARGET/settings.json
