252246eb229d38e5f8762bcc8fab4215c51c4550
Jason Barrie Morley <jbmorley@inseven.co.uk>
2013-01-04 17:44:15 +0000

    Adding copyright notices, removing a couple of defunct files and moving a few files into more logical locations.

--- src/js/gameboy.js
+++ src/js/gameboy.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2012-2013 InSeven Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 (function($) {
 
@@ -9,7 +26,7 @@
     IDLE: 0,
     LOADING: 1,
     RUNNING: 2,
-    ERROR: 3,
+    ERROR: 3
   };
 
   jQuery.extend(App.GameBoy.prototype, {
@@ -91,8 +108,6 @@
 
 })(jQuery);
 
-
-
 Gameboy = {};
 
 Gameboy.Key = {
@@ -103,7 +118,7 @@ Gameboy.Key = {
   UP: 38,
   DOWN: 40,
   LEFT: 37,
-  RIGHT: 39,
+  RIGHT: 39
 };
 
 function gb_Show_Fps() {}
