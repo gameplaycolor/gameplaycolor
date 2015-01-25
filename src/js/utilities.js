6e4c563269210defa6fd067a5c15d9e6b5311cae
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-01-25 13:18:25 +0100

    Adding a license to utilities.

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2012-2013 InSeven Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 var utilities = {
 
     encode_utf8: function(s) {
@@ -36,5 +54,5 @@ var utilities = {
         return c.charCodeAt(0); }));
       return u8_2;
     }
-    
+
 };
