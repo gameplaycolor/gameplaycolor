5a244eed3f9e390f69214295cb27bc62f544e706
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-05-18 09:51:47 +0200

    Updating the version in the logs.

--- src/js/app.js
+++ src/js/app.js
@@ -47,7 +47,7 @@
       self.device = device;
 
       self.logging = new App.Logging(App.Logging.Level.WARNING, "app");
-      self.logging.info("Version: 2.0.12");
+      self.logging.info("Version: 2.0.13");
       self.logging.info("Screen size: " + $(window).width() + " x " + $(window).height());
       self.logging.info("User Agent: " + navigator.userAgent);
 
