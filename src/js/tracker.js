b452797773b74b1d13468c2ce4e11cf3ad545694
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-06-03 17:15:12 +0200

    Fixing a bug in the tracker which was using the legacy settings.json.

--- src/js/tracker.js
+++ src/js/tracker.js
@@ -32,18 +32,16 @@ var _gaq = _gaq || [];
 
       if (window.navigator.onLine === true) {
 
-        jQuery.getJSON("settings.json", function(data) {
-          _gaq.push(['_setAccount', data.analytics]);
-          _gaq.push(['_setDomainName', data.domain]);
-          _gaq.push(['_trackPageview']);
+        _gaq.push(['_setAccount', window.config.analytics]);
+        _gaq.push(['_setDomainName', window.config.domain]);
+        _gaq.push(['_trackPageview']);
 
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
 
-        });
       }
 
     },
