d44df43877f4c1faf2bcf09f7e34acc31c7c45c0
Jason Barrie Morley <jason.morley@inseven.co.uk>
2016-03-20 06:09:51 +0000

    Reverting to the default settings defined in GameBoy-Online.

--- src/gbo/GameBoyIO.js
+++ src/gbo/GameBoyIO.js
@@ -7,14 +7,15 @@ var settings = [						//Some settings.
 	1,									//Volume level set.
 	true,								//Colorize GB mode?
 	false,								//Disallow typed arrays?
-	4,									//Interval for the emulator loop.
-	15,									//Audio buffer minimum span amount over x interpreter iterations.
-	30,									//Audio buffer maximum span amount over x interpreter iterations.
+	8,									//Interval for the emulator loop.
+	10,									//Audio buffer minimum span amount over x interpreter iterations.
+	20,									//Audio buffer maximum span amount over x interpreter iterations.
 	false,								//Override to allow for MBC1 instead of ROM only (compatibility for broken 3rd-party cartridges).
 	false,								//Override MBC RAM disabling and always allow reading and writing to the banks.
 	false,								//Use the GameBoy boot ROM instead of the GameBoy Color boot ROM.
 	false,								//Scale the canvas in JS, or let the browser scale the canvas?
-	true								//Use image smoothing based scaling?
+	true,								//Use image smoothing based scaling?
+    [true, true, true, true]            //User controlled channel enables.
 ];
 
 // Game Play modifications.
