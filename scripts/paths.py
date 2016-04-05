3a581a6369166a6789465da6d50ef1608d0ba169
Jason Barrie Morley <jason.morley@inseven.co.uk>
2016-04-05 05:58:53 +0200

    Introducing a new ansible deployment.

--- scripts/paths.py
+++ scripts/paths.py
@@ -2,4 +2,8 @@ import os.path
 
 SCRIPTS_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(SCRIPTS_DIR)
+SOURCE_DIR = os.path.join(ROOT_DIR, "src")
+ANSIBLE_DIR = os.path.join(ROOT_DIR, "ansible")
 BUILD_DIR = os.path.join(ROOT_DIR, "build")
+
+VERSION_FILE = os.path.join(SOURCE_DIR, "version.txt")
