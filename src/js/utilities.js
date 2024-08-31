273f6bdf751c89d18b8ec72128329d89577ec28c
Jason Morley <hello@jbmorley.co.uk>
2024-08-30 17:28:54 -1000

    Consolidation

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -47,13 +47,7 @@ var utilities = {
     },
 
     arrayBufferToBase64: function(buffer) {
-      let binary = '';
-      const bytes = new Uint8Array(buffer);
-      const len = bytes.byteLength;
-      for (let i = 0; i < len; i++) {
-        binary += String.fromCharCode(bytes[i]);
-      }
-      return window.btoa(binary);
+      return window.btoa(this.arrayBufferToBinaryString(buffer));
     },
 
     arrayToBase64: function(u8Arr) {
